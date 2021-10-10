de51aaf61b23a4754c504c287b0fa84bd8a24859
jun <hisonic001@naver.com>
2021-10-11 02:41:39 +0900

    2.0 User / Create Account

--- movies/movies.queries.js
+++
@@ -1,7 +0,0 @@
-import client from "../client";
-export default {
-  Query: {
-    movies: () => client.movie.findMany(), // Read => client를 사용해 movie data 검색
-    movie: (_, { id }) => client.movie.findUnique({ where: { id } }), // es6 문법 {id} == {id:id}
-  },
-};
